2520a57df3b5ddc91b3dff221f1318b1c35ace28
Lightziro <logantuscano@yandex.ru>
2021-03-04 20:55:07 +0300

    Converting a class to a class object

--- stockMarket.py
+++ stockMarket.py
@@ -1,28 +1,57 @@
 import datetime
 
-class stockMarket:
 
+class StockMarket():
     time = datetime.datetime.now()
-    typeMarket = []
-
-    ruMarket = {
-        'textButton': '🇷🇺 Российский рынок - сейчас',
-        'workTime': {
-            'workWith': datetime.datetime(time.year, time.month, time.day, 10, 1),
-            'workUntil': datetime.datetime(time.year, time.month, time.day, 18, 45),
+    workTimeMarket = {
+        'RUSSIA': {'WITH': datetime.datetime(time.year, time.month, time.day, 10, 1),
+                   'UNTIL': datetime.datetime(time.year, time.month, time.day, 18, 45)
+                   },
+        'USA': {
+            'WITH': datetime.datetime(time.year, time.month, time.day, 17, 30),
+            'UNTIL': datetime.datetime(time.year, time.month, time.day, 23, 45)
         },
-        'typeName': 'Russia',
-        'currentStock': ['Сбербанк', 'Газпром', 'Polymetal', 'ВТБ']
     }
-    usMarket = {
-        'textButton': '🇱🇷 Американский рынок - сейчас',
-        'workTime': {
-            'workWith': datetime.datetime(time.year, time.month, time.day, 17, 30),
-            'workUntil': datetime.datetime(time.year, time.month, time.day, 23, 45),
-        },
-        'typeName': 'USA',
-        'currentStock': ['Apple', 'Microsoft', 'Facebook', 'Tesla']
+    buttonMarket = {
+                       'USA': '🇱🇷 Американский рынок - сейчас',
+                       'RUSSIA': '🇷🇺 Российский рынок - сейчас'
+                   }
+    currentStockMarket = {
+        'USA': ['Apple', 'Microsoft', 'Facebook', 'Tesla'],
+        'RUSSIA': ['Сбербанк', 'Газпром', 'Polymetal', 'ВТБ'],
     }
 
-    def __init__(self):
-        self.typeMarket = [self.ruMarket, self.usMarket]
+    def __init__(self, typeMarket = None):
+        self.typeName = typeMarket
+        self.workTime = None
+        self.buttonText = None
+        self.currentStock = None
+        self.workTimeWith = None
+        self.workTimeUntil = None
+        self.settingsMarket()
+
+    def settingsMarket(self):
+        """
+        Configuring properties when initializing an object
+        """
+        self.getMarketWorkTime()
+        self.getButtonMarket()
+        self.getCurrentStock()
+
+    def getMarketWorkTime(self):
+        """
+        Assigns the exchange opening time to the object property
+        """
+        marketTime = self.workTimeMarket[self.typeName]
+        self.workTimeWith = marketTime['WITH']
+        self.workTimeUntil = marketTime['UNTIL']
+
+    def getCurrentStock(self):
+        """
+        Assigns the current promotions to the object property
+        """
+        self.currentStock = self.currentStockMarket[self.typeName]
+
+    def getButtonMarket(self):
+        self.buttonText = self.buttonMarket[self.typeName]
+
